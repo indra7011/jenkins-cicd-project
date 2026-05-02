bf5ae270a3f9837e2a8935b909095ca1084677f4
Indrajeet kumar <130952403+indra7011@users.noreply.github.com>
2026-05-02 15:40:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"Hello, this is my Jenkins CI/CD Pipeline project running successfullyfdsgsssssssssfffffafsaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd!")
+        self.wfile.write(b"Hello, this is my Jenkins CI/CD Pipeline project running successfullyfdsgsssssssssfffffafsaddddddddddddddddddddddddddddddddddddddddddddddddddddddd!")
 
 httpd = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
 print("Server running on port 80...")
