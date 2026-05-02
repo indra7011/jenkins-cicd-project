24433ce7ca30f6c10588f31d62e45ac6def26886
Indrajeet kumar <130952403+indra7011@users.noreply.github.com>
2026-05-02 12:33:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello, this is my Jenkins CI/CD Pipeline project running successfully!")
+
+httpd = HTTPServer(('0.0.0.0', 80), SimpleHTTPRequestHandler)
+print("Server running on port 80...")
+httpd.serve_forever()
